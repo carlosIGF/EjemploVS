05bb690a8c75f7a855dfb0094181b95dbed47ee4
carlosIGF <70751105+carlosIGF@users.noreply.github.com>
2020-11-09 09:25:23 +0100

    Update Program.cs

--- ProyectoFinalCiclo/ProyectoFinalCiclo/Program.cs
+++ ProyectoFinalCiclo/ProyectoFinalCiclo/Program.cs
@@ -8,7 +8,7 @@ namespace ProyectoFinalCiclo
         {
             Console.WriteLine("Hola clase");
             Console.ReadLine();
-            Console.WriteLine("Todos un 1");
+            Console.WriteLine("Todos un 10");
 
         }
     }
